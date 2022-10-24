83777dd7101148fb57f8551927b652abf59d4f6f
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:35:52 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/******************************************************************************
+                          HESAP MAKİNESİ KODLARI
+*******************************************************************************/
+
+
+class HelloWorld
+{
+    static void Main()
+    {
+        string tus;
+        do
+        {
+            Console.Write("İlk sayıyı giriniz : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Yapmak istediğini işlemi giriniz : ");
+            string islem = Console.ReadLine();
+
+            Console.Write("İkinci sayıyı giriniz : ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            switch (islem)
+            {
+                case "+": Console.WriteLine("Sonuç = {0}", (sayi1 + sayi2)); break;
+                case "-": Console.WriteLine("Sonuç = {0}", (sayi1 - sayi2)); break;
+                case "*": Console.WriteLine("Sonuç = {0}", (sayi1 * sayi2)); break;
+                case "/": Console.WriteLine("Sonuç = {0}", (sayi1 / sayi2)); break;
+                default: Console.WriteLine("Yanlış giriş yaptınız......"); break;
+            }
+            Console.WriteLine("Devam etmek için d tuşuna basınız....");
+            tus = Console.ReadLine();
+        }
+        while (tus == "d" || tus == "D");
+    }
+}
